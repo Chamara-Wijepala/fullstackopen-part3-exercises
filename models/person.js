b0787674bb4faae2b00c0985baabca4cb10447a5
Chamara-Wijepala <mr.worf777@gmail.com>
2023-03-02 19:06:06 +0530

    uibuild

--- models/person.js
+++ models/person.js
@@ -11,8 +11,23 @@ mongoose
   .catch((error) => console.log("connection error:", error.message));
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\d{2}|\d{3}-\d$/.test(v);
+      },
+      message: (props) =>
+        `${props.value} is not a valid phone number. It should be in the format of NN-NNNNNN or NNN-NNNNN`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
